d4d3845db574936bd0641503635e83866354a8fe
PaYengAnalytic <114193261+pyang0620@users.noreply.github.com>
2022-12-10 20:40:44 -0600

    Update Worksheet1.R

--- Worksheet1.R
+++ Worksheet1.R
@@ -1,26 +1,29 @@
-#Question 1 Worksheet 1
+#1.	Create a vector z as (3, -6, 7, 8); Create x as 10. Find z*x
 z<-c(3,-6,7,8)
 x<-10
 z*x
-#question 2
+#2.	Create a list as (3, z, “hello”)
 newlist<-list(3,z, "hello")
 newlist
-#question 3
+#3.	Create a matrix as
+3 5 6 
+4 7 8
+5 7 3
 A<-matrix(c(3,5,6,4,7,8,5,7,3), nrow=3, byrow = TRUE)
 A
-#question 4
+#4.	Create a table of data: 
 names<-c( "John","Peter","Julie")
 salary<-c(21000,23400,26800)
 startingdate<-c("2010-11-01", "2008-02-15", "2007-12-11")
 mydf<-data.frame(names,salary,startingdate)
-#question5
+#5.	Add a row of “Mary with salary of $50000, starting date of “2009-11-1” to the data frame in previous activity
 names<-"Mary"
 salary<-50000
 startingdate<-"2009-11-1"
 Mary<-data.frame(names,salary, startingdate)
 mydf2<-rbind(mydf,Mary)
 mydf2
-#question6 
+#6.	Add a column of gender to the above date frame (assign gender according to the names)
 gender<-c("Male","Male","Female", "Female")
 mydf3<-cbind(mydf2,gender)
 mydf3
